e5117dc3a8f8a7f14a715b400776f1624373baac
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2018-09-17 20:01:47 -0700

    working file upload

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import requests
+UPLOAD_FOLDER = '/tempfiles'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__, static_url_path="", static_folder="static")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
@@ -9,7 +19,7 @@ def main():
 
 @app.route("/upload")
 def upload():
-    return render_template("upload.html")
+    return render_template("upload.html", API_HOST="http://10.0.0.128:8088")
 
 
 if __name__ == "__main__":
