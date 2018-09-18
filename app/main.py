b9c523b8faf819fb945fe62431080bd5db1690eb
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2018-09-18 00:35:23 -0700

    add file listing to file storage page

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import requests
+import json
 UPLOAD_FOLDER = '/tempfiles'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__, static_url_path="", static_folder="static")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+API_HOST = "http://10.0.0.128:8088"
 
 
 def allowed_file(filename):
@@ -19,7 +22,10 @@ def main():
 
 @app.route("/upload")
 def upload():
-    return render_template("upload.html", API_HOST="http://10.0.0.128:8088")
+    uid = "0x4d409AB08C5B631A84dB907E4a916a7ea1375898"
+    items = requests.get(API_HOST+"/v2/items/"+uid)
+
+    return render_template("upload.html", API_HOST=API_HOST, items=items.json())
 
 
 if __name__ == "__main__":
