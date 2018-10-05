36b6a2d6868feed86a25e2a8bb8b3926544b5275
Brock Smedley <brocksmedley@gmail.com>
2018-10-05 01:10:50 +0000

    prod mode docker

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker stop ledgerfy
 docker rm ledgerfy
 
-docker run -it -p 8099:80 -p 80:80 --name ledgerfy --network vaas2_vaasnet ledgerfy
+docker run -dit -p 8099:80 -p 80:80 --name ledgerfy --network vaas2_vaasnet ledgerfy
